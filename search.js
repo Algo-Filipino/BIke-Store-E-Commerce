63dbebc31b5a20b1728e4bb3d0fa0e7f9b4becac
martin037 <estreramartin037@gmail.com>
2023-06-05 15:59:03 +0800

    initial commit

--- search.js
+++ search.js
@@ -1,53 +1,36 @@
-function handleFormSubmit(event) {
-  event.preventDefault(); // Prevent the form from submitting
-
-  // Get the search input value
+function handleSearch() {
   var searchInput = document.querySelector('.search-input');
-  var searchTerm = searchInput.value.toLowerCase(); // Convert the search term to lowercase for case-insensitive search
-
-  // Get the list of product titles
+  var searchTerm = searchInput.value.toLowerCase();
   var productTitles = document.querySelectorAll('.product-title');
 
+  // Clear previous search highlights
+  productTitles.forEach(function(title) {
+    title.style.backgroundColor = '';
+  });
+
   // Iterate over each product title and check for a match
-  var matchesFound = false;
   for (var i = 0; i < productTitles.length; i++) {
-    var productTitle = productTitles[i].textContent.toLowerCase(); // Convert the product title to lowercase
+    var productTitle = productTitles[i].textContent.toLowerCase();
 
     if (productTitle.includes(searchTerm)) {
       // Found a match, highlight the matched product title
-      productTitles[i].style.backgroundColor = 'lightgray';
-      matchesFound = true;
+      productTitles[i].style.backgroundColor = 'yellow';
 
       // Scroll to the matched product title
-      productTitles[i].scrollIntoView({ behavior: 'smooth', block: 'start' });
-    } else {
-      // Not a match, remove any previous highlighting
-      productTitles[i].style.backgroundColor = '';
+      productTitles[i].scrollIntoView({ behavior: 'smooth' });
+
+      // Clear the search input
+      searchInput.value = '';
+      return; // Exit the function after the first match is found
     }
   }
 
   // If no matches were found, display a message
-  if (!matchesFound) {
-    console.log('No matching products found.');
-  }
+  console.log('No matching products found.');
 
   // Clear the search input
   searchInput.value = '';
 }
 
-// Add an event listener to the form submission
-var searchForm = document.querySelector('.search-form');
-searchForm.addEventListener('submit', handleFormSubmit);
-
-// Make the search box sticky when typing
-var searchInput = document.querySelector('.search-input');
-searchInput.addEventListener('input', function(event) {
-  searchInput.classList.add('sticky');
-});
-
-// Remove sticky class when search box is empty
-searchInput.addEventListener('blur', function(event) {
-  if (searchInput.value === '') {
-    searchInput.classList.remove('sticky');
-  }
-});
+var searchButton = document.querySelector('.search-button');
+searchButton.addEventListener('click', handleSearch);
